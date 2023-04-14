e937f7da42501bba51f2a80681353f7be1d62c9d
marikf98 <marikf98@gmail.com>
2023-04-14 13:07:26 +0300

    Create GeneralCheckingFunctions.java

---
+++ ATP-Project-PartA/src/test/GeneralCheckingFunctions.java
@@ -0,0 +1,9 @@
+package test;
+
+public class GeneralCheckingFunctions {
+    public static String getGithubLink(){
+//change the <username> in the link to the username of the student who created the github project:
+        String githubLink = "https://github.com/marikf98/ATP-Project";
+        return githubLink;
+    }
+}

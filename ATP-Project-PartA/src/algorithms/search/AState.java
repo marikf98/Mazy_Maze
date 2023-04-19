afd4eaef03db2e821a641edfed73751c71e9b65c
marikf98 <marikf98@gmail.com>
2023-04-19 12:47:42 +0300

    started writing docstring

--- ATP-Project-PartA/src/algorithms/search/AState.java
+++ ATP-Project-PartA/src/algorithms/search/AState.java
@@ -2,38 +2,39 @@ package algorithms.search;
 
 import java.util.Comparator;
 
+/**This abstract lass represents a state in the search problem. it has the location as a String, a boolean visited filled
+ * so we can know it was searched already, and the cost of the move to this location, and the prev state that will hold the state that discovered this one **/
 public abstract class AState {
-
     private boolean visited;
     private AState prev;
     private int cost;
     private String location;
     private boolean m_isDiagonalMove;
+    private boolean isGoal;
 
-
+/** the constructor for the class**/
     public AState(String location) {
         this.location = location;
         this.prev = null;
         this.visited = false;
     }
 
-    private boolean isGoal;
+//    public void setGoal(boolean goal) {
+//        this.isGoal = goal;
+//    }
 
-
-    public void setGoal(boolean goal) {
-        this.isGoal = goal;
-    }
-
+    /**getter function for the visited field**/
     public boolean isVisited() {
         return visited;
     }
-
+    /** this setter lets us set that we visited this state**/
     public void setVisited() {
         this.visited = true;
     }
+    /** this setter lets us reset this state**/
 
     public void setUnVisited(){this.visited = false;}
-
+    /**getter and setter for the prev field**/
     public AState getPrev() {
         return prev;
     }
@@ -48,7 +49,7 @@ public abstract class AState {
             this.prev = null;
         }
     }
-
+    /**setter and getter for the cost field**/
     public int getCost() {
         return cost;
     }
@@ -56,10 +57,12 @@ public abstract class AState {
     public void setCost(int cost) {
         this.cost = cost;
     }
-
+    /**getter and setter for the location field**/
     public String getLocation() {
         return location;
     }
+
+    /**this function checks if two states are equal comparing by their location and visited field**/
     public boolean equals(AState other)
     {
         if(this == other){return true;}
@@ -72,13 +75,12 @@ public abstract class AState {
 
     }
 
-
+    /**abstract setter and getter function for the diagonal field**/
     public abstract void setDiagonalMove();
     public abstract boolean getDiagonalMove();
 
     @Override
     public String toString() {
         return location;
-
     }
 }
--- ATP-Project-PartA/src/algorithms/search/ISearchable.java
+++ ATP-Project-PartA/src/algorithms/search/ISearchable.java
@@ -1,7 +1,11 @@
 package algorithms.search;
 
 import java.util.ArrayList;
-
+/**this if the interface that each searchable need to implement for it to be eligible to be searched using the search algorithms
+ * getStart - returns the start location of the searchable
+ * getGoal - returns the goal location of the searchable
+ * getAllPossibleStates - will return all the possible states that can be reached from the current state
+ * setUnvisited - lets reset the searchable**/
 public interface ISearchable {
     public AState getStart();
     public AState getGoal();

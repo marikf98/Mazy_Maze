9284cee8b399aab3a70bff3b1ccd102ec1bcb1b4
marikf98 <marikf98@gmail.com>
2023-05-14 12:41:20 +0300

    started compress and decompress

--- ATP-Project-PartB/src/IO/MyCompressorOutputStream.java
+++ ATP-Project-PartB/src/IO/MyCompressorOutputStream.java
@@ -1,4 +1,45 @@
 package IO;
 
-public class MyCompressorOutputStream {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
+
+public class MyCompressorOutputStream extends OutputStream {
+    private OutputStream out;
+
+    public MyCompressorOutputStream(OutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] bytes) throws IOException {
+        byte[] arrayToCompress = compress(bytes);
+        for (byte toCompress : arrayToCompress) {
+            write(toCompress);
+        }
+    }
+
+    public byte[] compress(byte[] bytes)
+    {
+        Deflater compressor = new Deflater(Deflater.BEST_COMPRESSION); // a bit slower compression time but a better compression
+        compressor.setInput(bytes);
+        compressor.finish();
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        byte[] readBuffer = new byte[1024];
+        int readCount = 0;
+        while (!compressor.finished()) {
+            readCount = compressor.deflate(readBuffer);
+            if (readCount > 0) {
+                bao.write(readBuffer, 0, readCount);
+            }
+        }
+        compressor.end();
+        return bao.toByteArray();
+    }
 }
--- ATP-Project-PartB/src/IO/MyDecompressorInputStream.java
+++ ATP-Project-PartB/src/IO/MyDecompressorInputStream.java
@@ -1,4 +1,42 @@
 package IO;
 
-public class MyDecompressorInputStream {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.DataFormatException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+
+public class MyDecompressorInputStream extends InputStream {
+    private InputStream in;
+
+    public MyDecompressorInputStream(InputStream in) {
+        this.in = in;
+    }
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public int read(byte[] bytes) throws IOException
+    {
+        return 0;
+    }
+
+    public byte[] decompress(byte[] compressed) throws IOException, DataFormatException {
+        ByteArrayOutputStream decompresedArray = null;
+        Inflater decompressor = new Inflater();
+        decompressor.setInput(compressed);
+        decompresedArray = new ByteArrayOutputStream();
+        byte[] tmp = new byte[4*1024];
+        while(!decompressor.finished())
+        {
+            int size = decompressor.inflate(tmp);
+            decompresedArray.write(tmp, 0, size);
+        }
+        decompresedArray.close();
+        return decompresedArray.toByteArray();
+    }
 }

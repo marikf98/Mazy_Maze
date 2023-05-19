f7433eb4e46f120d133210299da2cdebf4d1f5a3
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-05-19 17:20:43 +0300

    update server and steart to implemnt it

--- ATP-Project-PartB/src/Server/ServerStrategyGenerateMaze.java
+++ ATP-Project-PartB/src/Server/ServerStrategyGenerateMaze.java
@@ -1,4 +1,46 @@
 package Server;
 
-public class ServerStrategyGenerateMaze {
+import algorithms.mazeGenerators.*;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+
+
+//mplements the IServerStrategy interface,
+// indicating that it defines the specific behavior .
+public class ServerStrategyGenerateMaze implements IServerStrategy {
+
+
+    @Override
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+
+            ArrayList<Integer> al = (ArrayList<Integer>) fromClient.readObject();
+            MyMazeGenerator myMazeGenerator=new MyMazeGenerator();
+            Maze maze =myMazeGenerator.generate(al.get(0),al.get(1));
+
+
+
+
+
+
+
+
+
+
+            toClient.writeObject(al);
+            toClient.flush();
+
+            fromClient.close();
+            toClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
